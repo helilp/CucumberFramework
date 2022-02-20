901acf8a980e6accb839771e5c20b03f27c7be6f
helilp <helilprimetech@gmail.com>
2022-02-20 12:15:31 -0500

    created testNG api tests and xml file

--- src/test/java/apiTests/PetStoreAPI_Tests.java
+++ src/test/java/apiTests/PetStoreAPI_Tests.java
@@ -2,14 +2,13 @@ package apiTests;
 
 import java.io.File;
 import java.io.FileReader;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;
 import org.hamcrest.Matchers;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import com.google.common.io.Files;
-import com.google.gson.JsonParser;
-
-import static org.hamcrest.Matchers.*;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
@@ -18,7 +17,7 @@ import static io.restassured.RestAssured.*;
 public class PetStoreAPI_Tests {
 	
 	
-	/*
+	/* 
 	 * Scenario: As a user, I should be able to perform POST request to add new pet to store 
 	 * Given I have the POST request URL and valid request body 
 	 * When I perform POST request to URL with request body 
@@ -30,17 +29,52 @@ public class PetStoreAPI_Tests {
 	String requestURL;
 	File requestBody;
 	Response response;
+	FileReader readJsonFile;
+	JSONParser parser;
+	JSONObject jsonObj;
+    int petID;
+    
+    
+    @BeforeMethod
+    public void beforeMethod() throws IOException, ParseException {
+       requestBody = new File("./src/test/resources/jsonFiles/addAPet.json");
+       readJsonFile = new FileReader(requestBody);
+  	   parser = new JSONParser();
+  	   jsonObj = (JSONObject) parser.parse(readJsonFile);
+  	  Long longPetId = (Long) jsonObj.get("id");
+  	  petID = longPetId.intValue();
+    }
 	
   @Test
-  public void createAPet() {
+  public void createAPet() throws IOException, ParseException {
 	  requestURL = "https://petstore.swagger.io/v2/pet";
-	  requestBody = new File("./src/test/resources/jsonFiles/addAPet.json");
-	  
+	  System.out.println(jsonObj.get("name"));
+	  System.out.println(jsonObj.get("status"));
 	  response = given().contentType(ContentType.JSON).accept(ContentType.JSON).body(requestBody)
 	  .when().post(requestURL);
 	  
 	  response.then().assertThat().contentType("application/json")
-	  .and().assertThat().statusCode(200);
-	  
+	  .and().assertThat().statusCode(200)
+	  .and().assertThat().body("id", Matchers.equalTo(petID))
+	  .and().assertThat().body("name", Matchers.equalTo(jsonObj.get("name")))
+	  .and().assertThat().body("status", Matchers.equalTo(jsonObj.get("status")));
   }
+  
+  @Test()
+  public void getPetByID() {
+	  requestURL = "https://petstore.swagger.io/v2/pet/";
+	  
+	  given().accept(ContentType.JSON)
+	  .when().get(requestURL + petID)
+	  .then().assertThat().contentType("application/json")
+	  .and().assertThat().statusCode(200)
+	  .and().assertThat().body("id", Matchers.equalTo(petID))
+	  .and().assertThat().body("name", Matchers.equalTo("Fluffy"))
+	  .and().assertThat().body("status", Matchers.equalTo("available"));
+	    
+  }
+  
+  
+  
+  
 }
